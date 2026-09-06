b148b7149a375161fbc026caee5886dbc78671cb
Akira Sato <x_lover24@yahoo.co.jp>
2026-09-06 17:07:44 +0900

    タスク編集・削除機能の実装

--- src/features/tasks/containers/TaskDetailContainer.tsx
+++ src/features/tasks/containers/TaskDetailContainer.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { getTaskById } from "../hooks/getTaskById";
 import type { Task } from "../../../types/task";
 import TaskDetail from "../components/TaskDetail"
+import TaskDeleteButton from "../components/TaskDeleteButton";
 import styles from "../styles/taskDetail.module.css"
 
 function TaskDetailContainer() {
@@ -38,7 +39,10 @@ function TaskDetailContainer() {
 
      return (
         <div className={styles.taskDetailContainer}>
-            <TaskDetail task={task} />{/* 取得したタスク情報をTaskDetailコンポーネントに渡す */}
+            <TaskDetail task={task} />
+            <div>
+                <TaskDeleteButton id={id} />
+            </div>
         </div>
      )
 }
